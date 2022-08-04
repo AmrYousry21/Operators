9a09dcba5073afd9d9e9fc2249b4f0bc4236ae2c
Amr Yousry <amryousry19@gmail.com>
2022-08-04 14:52:02 -0400

    Operator Exercises

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();    
+
+            var a = 17;
+            var b = 4;
+            
+
+            program.OperatorExercise(a, b);
+
+            Console.WriteLine("Enter the radius");
+            var r = double.Parse(Console.ReadLine());
+
+            program.CalculateTheArea(r);
+        }
+
+        public void OperatorExercise(int a,  int b) 
+        {
+            int division = a / b;
+            int remainder = a % b;
+            
+            Console.WriteLine($"{a} + {b} = {a+b}");
+            Console.WriteLine($"{a} - {b} = {a - b}");
+            Console.WriteLine($"{a} * {b} = {a * b}");
+            Console.WriteLine($"{a} / {b} is {division} remainder {remainder}");
+
+        }
+
+        public void CalculateTheArea(double r) 
+        {
+
+            double pi = Math.PI;
+            Console.WriteLine($"Area of the circle = {pi * r * r}");
+        }
+
+
+    }
+}
